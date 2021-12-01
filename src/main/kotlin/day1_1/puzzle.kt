de53b22548d9477ba005b959f94543fee466f7c3
Sigyn <shadow@sigyn.dev>
2021-12-01 11:22:13 +0000

    day1_1

--- src/main/kotlin/day1_1/puzzle.kt
+++ src/main/kotlin/day1_1/puzzle.kt
@@ -3,13 +3,12 @@ package day1_1
 import java.io.File
 
 fun main() {
-    val input = File("src/Day1_1/input").readLines().map { it.toInt()  }
-    for (i in input) {
-        for (j in input) {
-            if (i + j == 2020) {
-                println(i * j)
-                return
+    val input = File("src/main/kotlin/day1_1/input").readLines().map { it.toInt()  }
+    var largerMeasurementCount = 0;
+    for (i in 1 until input.size) {
+            if (input[i] > input[i - 1]) {
+                largerMeasurementCount++
             }
-        }
     }
+    println(largerMeasurementCount)
 }
