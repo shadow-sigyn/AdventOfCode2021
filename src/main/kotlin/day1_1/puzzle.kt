c6b5043008c6ce230be438e5ec07f0d942f3339f
Sigyn <shadow@sigyn.dev>
2021-12-01 12:05:38 +0000

    day1_2

--- src/main/kotlin/day1_1/puzzle.kt
+++ src/main/kotlin/day1_1/puzzle.kt
@@ -4,7 +4,7 @@ import java.io.File
 
 fun main() {
     val input = File("src/main/kotlin/day1_1/input").readLines().map { it.toInt()  }
-    var largerMeasurementCount = 0;
+    var largerMeasurementCount = 0
     for (i in 1 until input.size) {
             if (input[i] > input[i - 1]) {
                 largerMeasurementCount++
--- src/main/kotlin/day1_2/puzzle.kt
+++ src/main/kotlin/day1_2/puzzle.kt
@@ -1,2 +1,16 @@
 package day1_2
 
+import java.io.File
+
+fun main() {
+    val input = File("src/main/kotlin/day1_1/input").readLines().map { it.toInt() }
+    var largerMeasurementCount = 0
+    for (i in 3 until input.size) {
+        val window1 = input[i - 3] + input[i - 2] + input[i - 1]
+        val window2 = input[i - 2] + input[i - 1] + input[i]
+        if (window1 < window2) {
+            largerMeasurementCount++
+        }
+    }
+    println(largerMeasurementCount)
+}
