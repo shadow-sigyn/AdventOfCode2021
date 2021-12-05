a6793f0b2cf27e25fb56f0f6c969145ef105d931
Sigyn <shadow@sigyn.dev>
2021-12-05 18:17:35 +0000

    day2_1

--- src/main/kotlin/day2_1/puzzle.kt
+++ src/main/kotlin/day2_1/puzzle.kt
@@ -1,2 +1,24 @@
 package day2_1
 
+import java.io.File
+
+class Position {
+    var x = 0
+    var y = 0
+}
+
+fun main() {
+    val input = File("src/main/kotlin/day2_1/input").readLines()
+    var position = Position()
+
+    for (line in input) {
+        if (line.contains("forward")) {
+            position.x += line.filter { it.isDigit() }.toInt()
+        } else if (line.contains("down")) {
+            position.y += line.filter { it.isDigit() }.toInt()
+        } else if (line.contains("up")) {
+            position.y -= line.filter { it.isDigit() }.toInt()
+        }
+    }
+    println(position.x * position.y)
+}
